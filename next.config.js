b2daed51acb1f350cc3bd67bcbe40badf7c63353
André Antunes <andrem.ferraz@hotmail.com>
2022-04-22 03:53:53 -0300

    new version

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = {nextConfig}
-
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = {nextConfig}
