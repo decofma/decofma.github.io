30da236bb8dac3f00c1923fc3107b6f093818548
André Antunes <andrem.ferraz@hotmail.com>
2022-04-23 19:42:19 -0300

    new test

--- next.config.js
+++ next.config.js
@@ -6,18 +6,10 @@
 // module.exports = {nextConfig}
 module.exports = {
   images: {
-    loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    nextImageExportOptimizer: {
-      imageFolderPath: "public",
-      exportFolderPath: "out",
-      quality: 75,
-    },
+    loader: "imgix",
+    path: "https://noop/",
   },
-  env: {
-    storePicturesInWEBP: true,
-  },
+  
   assetPrefix: './',
   // basePath: '/docs',
   reactStrictMode: true,
