c6c1499ad1636a983494162cc2f551c029de6819
André Antunes <andrem.ferraz@hotmail.com>
2022-04-23 21:36:58 -0300

    new index for cloudinary

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,7 +55,7 @@ export default function Home() {
       <main className={styles.main}>
         
         <div id="home">
-            <Image  src="me_fuyhxy.png" alt="yep, that's me" width={1000} height={700} layout={"fixed"} quality={100} />
+            <Image  src="me.png" alt="yep, that's me" width={1000} height={700} layout={"fixed"} quality={100} />
         </div>
 
         <h1 className={styles.title}>
